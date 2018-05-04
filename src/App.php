664fdc0884713ec592933f13ce806d7a5b507f10
Joelson Batista <joelson@ferimport.com.br>
2018-05-04 13:40:06 -0300

    added App and Response

---
+++ src/App.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace MonitoLib;
+
+class App
+{
+    static private $instance;
+    static private $isWindows;
+    static private $cachePath;
+    static private $configPath;
+    static private $logPath;
+    static private $rootPath;
+    static private $storagePath;
+    static private $tmpPath;
+
+    private function __construct ()
+    {
+    }
+    public static function getInstance ()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new \MonitoLib\App;
+        }
+
+        return self::$instance;
+    }
+    private static function getPath ($directory, $relativePath = null)
+    {
+        $directoryPath = $directory . 'Path'; 
+
+        if (is_null(self::$$directoryPath)) {
+            $path = self::$rootPath . $directory . DIRECTORY_SEPARATOR;
+
+            if (!file_exists($path)) {
+                if (!mkdir($path, 0755, true)) {
+                    throw new \Exception("Error creating directory $path", 1);
+                }
+            }
+
+            self::$$directoryPath = $path;
+        }
+
+        if (!is_null($relativePath)) {
+            $relativePath = self::$$directoryPath . $relativePath . DIRECTORY_SEPARATOR;
+
+            if (!file_exists($relativePath)) {
+                if (!mkdir($relativePath, 0755, true)) {
+                    throw new \Exception("Error creating directory $relativePath", 1);
+                }
+            }
+
+            return $relativePath;
+        }
+
+        return self::$$directoryPath;
+    }
+    public static function getCachePath ($relativePath = null)
+    {
+        return self::getPath('cache', $relativePath);
+    }
+    public static function getConfigPath ($relativePath = null)
+    {
+        return self::getPath('config', $relativePath);
+    }
+    public static function getLogPath ($relativePath = null)
+    {
+        return self::getPath('log', $relativePath);
+    }
+    public static function getStoragePath ($relativePath = null)
+    {
+        return self::getPath('storage', $relativePath);
+    }
+    public static function getRootPath ()
+    {
+        return self::$rootPath;
+    }
+    public static function getTmpPath ($relativePath = null)
+    {
+        return self::getPath('tmp', $relativePath);
+    }
+    public static function isWindows ()
+    {
+        return self::$isWindows;
+    }
+    public static function setRootPath ($rootPath)
+    {
+        if (is_null(self::$rootPath)) {
+            self::$rootPath = $rootPath;
+        }
+    }
+}
---
+++ src/Exception/Api.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Exception handler
+ * @author Joelson B <joelsonb@msn.com>
+ * @copyright Copyright &copy; 2018
+ *  
+ * @package MonitoLib
+ */
+namespace MonitoLib\Exception;
+
+class Api extends \Exception
+{
+    // protected $message = 'Unknown exception3';   // exception message
+    private   $string;                          // __toString cache
+    protected $code = 0;                        // user defined exception code
+    protected $file;                            // source filename of exception
+    protected $line;                            // source line of exception
+    private   $trace;                           // backtrace
+    private   $previous;                        // previous exception if nested exception
+    private $errors = ['k'];
+
+    public function __construct ($message = null, $errors = null, $code = 0, \Exception $previous = null)
+    {
+        $this->errors = $errors;
+        parent::__construct($message, $code, $previous);
+    }
+
+    // final private function __clone();           // Inhibits cloning of exceptions.
+
+    // final public function getMessage()        // message of exception
+    // {
+    //     return parent::getMessage();
+    // }
+    // final public function getCode()           // code of exception
+    // {
+    //     return parent::getCode();
+    // }
+    // final public function getFile()           // source filename
+    // {
+    //     return parent::getFile();
+    // }
+    // final public function getLine()           // source line
+    // {
+    //     return parent::getLine();
+    // }
+    // final public function getTrace()          // an array of the backtrace()
+    // {
+    //     return parent::getTrace();
+    // }
+    // final public function getPrevious()       // previous exception
+    // {
+    //     return parent::getPrevious();
+    // }
+    // final public function getTraceAsString()  // formatted string of trace
+    // {
+    //     return parent::getTraceAsString();
+    // }
+
+    // Overrideable
+    public function __toString ()               // formatted string for display
+    {
+        return $this->errors;
+    }
+    public function getErrors ()
+    {
+        return $this->errors;
+    }
+}
